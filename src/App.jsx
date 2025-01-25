1e4ee54886d6dc312d86ce8fca4d76a198fa01ba
Mohammad Amin Rajabi <mohammadaminrajabii2004@gmail.com>
2025-01-25 12:12:31 +0330

    [ADD] Not Found Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import NewPostPage from "./pages/NewPostPage";
+import NotFound from "./pages/Not-Found";
+import "./App.css";
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/new-post" element={<NewPostPage />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
