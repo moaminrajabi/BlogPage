2d513e6ae8870cbbddaf389d2748e99ea0979921
Mohammad Amin Rajabi <mohammadaminrajabii2004@gmail.com>
2025-01-26 16:32:50 +0330

    [ADD] Search post feuter

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,20 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { getPosts } from "../data/PostData";
 import PostCard from "../components/PostCard";
 import { Link } from "react-router-dom";
 
 function HomePage() {
+  const [searchQuery, setSearchQuery] = useState("");
   const posts = getPosts();
+
+  const filterdPosts = posts.filter(
+    (post) =>
+      post.title.toLowerCase().includes(searchQuery.toLowerCase()) ||
+      post.description.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
   return (
     <div>
       <header>
-        <input className="input" type="text" placeholder="Search..." />
+        <input
+          className="input"
+          type="text"
+          placeholder="Search..."
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
         <Link className="button" to="/new-post">
           Add New Post
         </Link>
       </header>
       <div className="Home">
-        {posts.map((post) => (
+        {filterdPosts.map((post) => (
           <PostCard key={post.id} {...post} />
         ))}
       </div>
